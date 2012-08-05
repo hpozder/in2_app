baead3496abcf1eb9cfa7003ae837e550a1c6f21
hpozder <hrvojepozder@gmail.com>
2012-08-06 01:05:34 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 In2App::Application.routes.draw do
   
+  get "users/new"
+
   root :to => 'static_pages#home'
+  
+  match '/signup',  :to => 'users#new'
   
   match '/help',    :to => 'static_pages#help'
   match '/about',   :to => 'static_pages#about'
