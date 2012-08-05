85f649e604ac6a1fb7e7d058d2d991d1347d4e37
hpozder <hrvojepozder@gmail.com>
2012-08-06 00:55:21 +0200

    Finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,19 @@
 class StaticPagesController < ApplicationController
   
   def home
+    #@page_title = "Sample App"
   end
 
   def help
+    #@page_title = "Help"
   end
   
   def about
+    #@page_title = "About Us"
+  end
+  
+  def contact
+    #@page_title = "Contact"
   end
   
 end
