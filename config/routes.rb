7ebd0a4550b5275ceabdc8522d7cc99487e5bd7a
hpozder <hrvojepozder@gmail.com>
2012-08-07 00:31:14 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 In2App::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'static_pages#home'
   
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy', :via => :delete
   
   match '/help',    :to => 'static_pages#help'
   match '/about',   :to => 'static_pages#about'
