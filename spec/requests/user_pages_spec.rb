ef17c13f8d2681113777e3cd735caf7a7c23b17d
hpozder <hrvojepozder@gmail.com>
2012-08-10 19:36:44 +0200

    Finish destroy action

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,7 +11,7 @@ describe "User pages" do
     before(:all) { 10.times { FactoryGirl.create(:user) } }
     after(:all)  { User.delete_all }
 
-    before(:each) do
+    before do
       sign_in user
       visit users_path
     end
@@ -25,6 +25,26 @@ describe "User pages" do
 
       
     end
+	
+	describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', :href => user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', :href => user_path(admin)) }
+      end
+    end
+	
   end
   
   describe "profile page" do
